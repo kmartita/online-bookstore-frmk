7514ed3a73d32865c1710b750f103ffabe31c3ec
Marta Kravchuk <martakravchuk@Martas-MacBook-Pro.local>
2025-06-10 17:15:40 +0300

    Initial commit

---
+++ src/main/java/com/allwyn/App.java
@@ -0,0 +1,13 @@
+package com.allwyn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
