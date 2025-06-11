15c5ebba615d9e5c7739e079a8b46fdb7936203f
Marta Kravchuk <martakravchuk@Martas-MacBook-Pro.local>
2025-06-11 19:21:24 +0300

    minor fix with BASE_URL

--- src/main/java/com/allwyn/tools/EnvManagerUtil.java
+++ src/main/java/com/allwyn/tools/EnvManagerUtil.java
@@ -6,11 +6,11 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class EnvManagerUtil {
 
-    private static final Dotenv ENV = Dotenv
+    /*private static final Dotenv ENV = Dotenv
             .configure()
             .ignoreIfMissing()
             .load();
 
-    public static final String BASE_URL = (ENV.get("BASE_URL") != null) ? ENV.get("BASE_URL") : System.getenv("BASE_URL");
+    public static final String BASE_URL = (ENV.get("BASE_URL") != null) ? ENV.get("BASE_URL") : System.getenv("BASE_URL");*/
 
 }
